ae2e382170f823bb5013311fe99e5523ca6d7186
SJaffa <sarah.jaffa@astro.cf.ac.uk>
2017-12-14 13:17:11 +0000

    Bug fix break when defined

--- gridrecon.py
+++ gridrecon.py
@@ -81,67 +81,70 @@ llist.append(l1)
 p1=pcs[0,0]
 p2=pcs[1,0]
 
-print "PC1 = %3.2f, PC2 = %3.2f"%(p1,p2)
-if p2<-0.2:
-    print "This cluster is on the edge of parameter space \nand may not be fractally substructured: PC2<-0.2"
+while 0:
 
-try:
-    thissquare,_=cf.find_square(grid_info,p1,p2)
-except TypeError:
-    fout.write("Outside parameter space:")
-    fout.write("PC1=%3.2f, PC2=%3.2f\n"%(p1,p2))
-    exit
-
-est=grid_info[thissquare]
-
-if est.N>2: 
-    fout.write(est.printD())
-    fout.write(est.printG())
-    g_found=est.G.mean
-    d_found=est.D.mean
-else:
-    neighbs,newN=cf.find_neighbs(grid_info,thissquare)
-    if newN==0:
-        fout.write("Outside parameter space (empty super-square):")
-        fout.write("PC1=%3.2f, PC2=%3.2f\n"%(p1,p2))
-        exit
-    #merge D
-    thisN=grid_info[neighbs[0]].N
-    thisDmean=grid_info[neighbs[0]].D.mean
-    thisDvar=grid_info[neighbs[0]].D.std
-    for i in range(1,len(neighbs)):
-        thisstuff=[thisDmean,thisDvar,thisN]
-        newstuff=[grid_info[neighbs[i]].D.mean,
-                  grid_info[neighbs[i]].D.std,
-                  grid_info[neighbs[i]].N]
-        thisDmean,thisDvar,thisN=cf.pooledmeanvar(thisstuff,newstuff)
-    fout.write("[%3.2f,%3.2f],"%(thisDmean,thisDvar))
+    print "PC1 = %3.2f, PC2 = %3.2f"%(p1,p2)
+    if p2<-0.2:
+        print "This cluster is on the edge of parameter space \nand may not be fractally substructured: PC2<-0.2"
     
-    #merge G
-    thisN=grid_info[neighbs[0]].N
-    thisGmean=grid_info[neighbs[0]].G.mean
-    thisGvar=grid_info[neighbs[0]].G.std
-    for i in range(1,len(neighbs)):
-        thisstuff=[thisGmean,thisGvar,thisN]
-        newstuff=[grid_info[neighbs[i]].G.mean,
-                  grid_info[neighbs[i]].G.std,
-                  grid_info[neighbs[i]].N]
-        thisGmean,thisGvar,thisN=cf.pooledmeanvar(thisstuff,newstuff)
-    fout.write("[%3.2f,%3.2f],"%(thisGmean,thisGvar))
-    g_found=thisGmean
-    d_found=thisDmean
-#look up C using A measure
-#find nearest D,G
-f_found=2**d_found
-g_found=round(g_found)
-f_found=round(f_found)
-d_found=np.log2(f_found)
-#print A_measure
-c_mean,c_std=cf.find_c(d_found,g_found,A_measure)
-#fout.write("[%3.2f,%3.2f]],\n"%(c_mean,c_std))
-fout.write("[%3.2f,%3.2f]\n"%(1./c_mean,(c_std/(c_mean**2))))
-
-ii+=1
+    try:
+        thissquare,_=cf.find_square(grid_info,p1,p2)
+    except TypeError:
+        fout.write("Outside parameter space:")
+        fout.write("PC1=%3.2f, PC2=%3.2f\n"%(p1,p2))
+        break
+    
+    est=grid_info[thissquare]
+    
+    if est.N>2: 
+        fout.write(est.printD())
+        fout.write(est.printG())
+        g_found=est.G.mean
+        d_found=est.D.mean
+    else:
+        neighbs,newN=cf.find_neighbs(grid_info,thissquare)
+        if newN==0:
+            fout.write("Outside parameter space (empty super-square):")
+            fout.write("PC1=%3.2f, PC2=%3.2f\n"%(p1,p2))
+            exit
+        #merge D
+        thisN=grid_info[neighbs[0]].N
+        thisDmean=grid_info[neighbs[0]].D.mean
+        thisDvar=grid_info[neighbs[0]].D.std
+        for i in range(1,len(neighbs)):
+            thisstuff=[thisDmean,thisDvar,thisN]
+            newstuff=[grid_info[neighbs[i]].D.mean,
+                      grid_info[neighbs[i]].D.std,
+                      grid_info[neighbs[i]].N]
+            thisDmean,thisDvar,thisN=cf.pooledmeanvar(thisstuff,newstuff)
+        fout.write("[%3.2f,%3.2f],"%(thisDmean,thisDvar))
+        
+        #merge G
+        thisN=grid_info[neighbs[0]].N
+        thisGmean=grid_info[neighbs[0]].G.mean
+        thisGvar=grid_info[neighbs[0]].G.std
+        for i in range(1,len(neighbs)):
+            thisstuff=[thisGmean,thisGvar,thisN]
+            newstuff=[grid_info[neighbs[i]].G.mean,
+                      grid_info[neighbs[i]].G.std,
+                      grid_info[neighbs[i]].N]
+            thisGmean,thisGvar,thisN=cf.pooledmeanvar(thisstuff,newstuff)
+        fout.write("[%3.2f,%3.2f],"%(thisGmean,thisGvar))
+        g_found=thisGmean
+        d_found=thisDmean
+    #look up C using A measure
+    #find nearest D,G
+    f_found=2**d_found
+    g_found=round(g_found)
+    f_found=round(f_found)
+    d_found=np.log2(f_found)
+    #print A_measure
+    c_mean,c_std=cf.find_c(d_found,g_found,A_measure)
+    #fout.write("[%3.2f,%3.2f]],\n"%(c_mean,c_std))
+    fout.write("[%3.2f,%3.2f]\n"%(1./c_mean,(c_std/(c_mean**2))))
+    
+    ii+=1
+    break
 
 #except MemoryError:
 #    continue
